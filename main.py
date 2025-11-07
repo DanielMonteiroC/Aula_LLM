042417e6857b725f58a5a7e8b550242f50da0475
Daniel Monteiro <danielmonteirodcarvalho@gmail.com>
2025-11-06 21:38:51 -0300

    3 commit

--- main.py
+++ main.py
@@ -12,10 +12,36 @@ import ollama
 
 # -- Chamando keys do .env --
 openai_Cliente = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-anthropic_Cliente = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
-#xai_Cliente = xai_sdk.Client(api_key=os.getenv("XAI_API_KEY"))
-gemini.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-groq_Cliente = groq.Groq(api_key=os.getenv("GROQ_API_KEY"))
+# anthropic_Cliente = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+# xai_Cliente = xai_sdk.Client(api_key=os.getenv("XAI_API_KEY"))
+# gemini.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# groq_Cliente = groq.Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-print("Todas as configurações foram feitas com sucesso!")
+response = openai_Cliente.responses.create(
+    model='gpt-5-nano',
+    reasoning={
+        "effort":"low"
+    },
+    input=[
+        {
+            "role":"system",
+            "content":[
+                {
+                    "type":"input_text",
+                    "text":"Seja direto e consiso, use uma unica frase nas respostas"
+                }
+            ]
+        },
+        {
+            "role":"user",
+            "content":[
+                {
+                    "type":"input_text",
+                    "text":"Qual o sentido da vida?"
+                }
+            ]
+        }
+    ]
+)
 
+print(response.output_text)
